245c4aa2b976c497192bc2a5a85af7b35d1f04a8
Juan Cabrera <yesuscabrera2003@gmail.com>
2025-02-13 17:41:48 -0600

    Add description on the new columns

---
+++ Web_app/Home.py
@@ -0,0 +1,21 @@
+import streamlit as st 
+
+st.set_page_config(layout="wide")
+
+col1, col2 = st.columns(2)
+
+with col1:
+    st.image("Web_app/Me.jpg", width=600)
+
+with col2:
+    st.title("Juan Cabrera")
+    text = """
+    Hello this my first porfolio.
+
+    """
+    st.info(text)
+
+text2 = """Below you can find some of the apps
+        I have built in python. Feel free to contact me
+        """
+st.write(text2)
